1e5904d2c3b5c941a24ca3ed1b37e867072cedc6
Alexander Scherbatiy <alexander.scherbatiy@yandex.com>
2024-09-24 10:28:04 +0300

    Add electron and proton particles

--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
-console.log("Hello, Physics Tables")
+
+const PARTICLE_NAME = "PARTICLE_NAME"
+const PARTICLE_MASS = "PARTICLE_MASS" // kg
+
+class Particle {
+    public properties: Map<string, any>
+    constructor(props: [key: string, value: any][]) {
+        this.properties = new Map<string, any>(props)
+    }
+}
+
+const particles: Particle[] = [
+    new Particle([
+        [PARTICLE_NAME, "electron"],
+        [PARTICLE_MASS, 9.1e-31],
+    ]),
+    new Particle([
+        [PARTICLE_NAME, "proton"],
+        [PARTICLE_MASS, 1.7e-27],
+    ]),
+]
+
+const map2 = new Map<string, string>([["one", "1"], ["two", "two"]])
+
+for (const particle of particles) {
+    for (const [key, value] of particle.properties) {
+        console.log(`key: ${key}, value: ${value}`)
+    }
+}
