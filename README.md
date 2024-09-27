3e6ee0577688fa7e2b6613a60b5aeb2420a7aecb
Alexander Scherbatiy <alexander.scherbatiy@yandex.com>
2024-09-27 08:59:56 +0300

    Write physics tables to README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# physics-tables
-Physics Tables
+# Phyiscs Tables
+## Elementary particles
+| particle / mass  |kg|
+|------------|------------|
+|electron    |9.11e-31    |
+|proton      |1.67e-27    |
+|neutron     |1.68e-27    |
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { writeFileSync } from 'fs';
 
 const PARTICLE_NAME = "PARTICLE_NAME"
 const PARTICLE_MASS = "PARTICLE_MASS" // kg
@@ -7,23 +8,64 @@ class Particle {
     constructor(props: [key: string, value: any][]) {
         this.properties = new Map<string, any>(props)
     }
+
+    get(key: string): any {
+        return this.properties.get(key)
+    }
 }
 
 const particles: Particle[] = [
     new Particle([
         [PARTICLE_NAME, "electron"],
-        [PARTICLE_MASS, 9.1e-31],
+        [PARTICLE_MASS, 9.11e-31],
     ]),
     new Particle([
         [PARTICLE_NAME, "proton"],
-        [PARTICLE_MASS, 1.7e-27],
+        [PARTICLE_MASS, 1.67e-27],
+    ]),
+    new Particle([
+        [PARTICLE_NAME, "neutron"],
+        [PARTICLE_MASS, 1.68e-27],
     ]),
 ]
 
-const map2 = new Map<string, string>([["one", "1"], ["two", "two"]])
-
+/*
 for (const particle of particles) {
     for (const [key, value] of particle.properties) {
         console.log(`key: ${key}, value: ${value}`)
+    }
+}
+*/
+
+class TableWriter {
+    private lines: string[] = []
+
+    write(line: string): void {
+        this.lines.push(line)
+    }
+
+    toFile(fileName: string): void {
+        writeFileSync(`./${fileName}`, this.lines.join("\n"));
+    }
+}
+
+const writer = new TableWriter()
+physicsTables(writer, particles)
+writer.toFile("README.md")
+
+function physicsTables(writer: TableWriter, particles: Particle[]) {
+    writer.write(`# Phyiscs Tables`)
+    elementaryParticlesTable(writer, particles)
+}
+
+function elementaryParticlesTable(writer: TableWriter, particles: Particle[]): void {
+    writer.write(`## Elementary particles`)
+    writer.write(`| particle / mass  |kg|`)
+    writer.write(`|------------|------------|`)
+    const pad = 12
+    for (const p of particles) {
+        const name = `${p.get(PARTICLE_NAME)}`.padEnd(pad)
+        const mass = `${p.get(PARTICLE_MASS)}`.padEnd(pad)
+        writer.write(`|${name}|${mass}|`)
     }
 }
